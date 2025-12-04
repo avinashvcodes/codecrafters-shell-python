15b40a44090458c6d631fed90c15971568f2e22e
Avinash <avinash.v@idp.com>
2025-12-04 16:23:41 +0530

    write test without flush

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    sys.stdout.flush()
+    # sys.stdout.flush()
 
     command = sys.stdin.readline()
     sys.stdout.write(f"{command}: command not found")
