9e204513beba896bf4805ae1aca320b6f5831959
Avinash <avinash.v@idp.com>
2025-12-06 18:18:21 +0530

    Run a program

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ def main():
         if cmd in BUILTINS:
             BUILTINS[cmd](args)
         elif exe:= shutil.which(cmd):
-            with subprocess.Popen([exe]+args) as sp:
+            with subprocess.Popen([exe]+[cmd]+args) as sp:
                 sp.wait()
         else:
             print(f"{cmd}: command not found")
