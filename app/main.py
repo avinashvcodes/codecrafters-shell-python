3e148860e16904769bc8dd7c24f637263fb96f16
Avinash <avinash.v@idp.com>
2025-12-07 15:34:29 +0530

    pwd

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import sys
 import shutil
+import os
 import subprocess
 
-BUILTIN_NAMES = {"exit", "echo", "type"}
+BUILTIN_NAMES = {"exit", "echo", "type", "pwd"}
 
 def echo(args):
     print(" ".join(args))
@@ -21,6 +22,7 @@ def get_type(cmds):
 
 BUILTINS = {
     "exit": lambda *_: sys.exit(),
+    "pwd": lambda *_: print(os.getcwd()),
     "echo": echo,
     "type": get_type
 }
